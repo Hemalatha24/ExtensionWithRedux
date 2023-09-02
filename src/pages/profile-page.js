3ec3a365208f4c2baa02dc33bc741907e2f8fcdd
Hemalatha24 <hemalatham06@gmail.com>
2023-09-02 07:12:58 +0530

    Update profile-page.js

--- src/pages/profile-page.js
+++ src/pages/profile-page.js
@@ -1,14 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React, {useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { profileSuccess } from "../actions/profileAction";
 import { CodeSnippet } from "../components/code-snippet";
 import { PageLayout } from "../components/page-layout";
 
 export const ProfilePage = () => {
-  const { user } = useAuth0();
 
+  const { user } = useAuth0();
+  const profile = useSelector((state) => state.profile);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(profileSuccess(user));
+ }, []);
   if (!user) {
     return null;
   }
+ 
 
   return (
     <PageLayout>
@@ -29,19 +37,19 @@ export const ProfilePage = () => {
           <div className="profile-grid">
             <div className="profile__header">
               <img
-                src={user.picture}
+                src={profile.picture}
                 alt="Profile"
                 className="profile__avatar"
               />
               <div className="profile__headline">
-                <h2 className="profile__title">{user.name}</h2>
-                <span className="profile__description">{user.email}</span>
+                <h2 className="profile__title">{profile.name}</h2>
+                <span className="profile__description">{profile.email}</span>
               </div>
             </div>
             <div className="profile__details">
               <CodeSnippet
                 title="Decoded ID Token"
-                code={JSON.stringify(user, null, 2)}
+                code={JSON.stringify(profile, null, 2)}
               />
             </div>
           </div>
